1d41bc170a6a5834649283a1154513b303d786ab
Dinesh Sinha <dinesh@vidyo.com>
2018-03-25 17:21:38 -0400

    Animations Part 1

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -9,12 +9,26 @@ import { Comment } from '../shared/comment';
 
 import 'rxjs/add/operator/switchMap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -27,6 +41,8 @@ export class DishdetailComponent implements OnInit {
 
   commentForm: FormGroup;
   comment: Comment;
+
+  visibility = 'shown';
 
 
   formErrors = {
@@ -75,7 +91,7 @@ export class DishdetailComponent implements OnInit {
       this.dishcopy.comments.push(this.comment);
       this.dishcopy.save()
         .subscribe(dish => { this.dish = dish; console.log(this.dish); });
-        
+
       this.addToList(this.comment);
       this.commentForm.reset({
         rating: 5,
@@ -89,9 +105,11 @@ export class DishdetailComponent implements OnInit {
 
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
+      .switchMap((params: Params) => {this.visibility = 'hidden'; 
+                return this.dishservice.getDish(+params['id'])})
       .subscribe(dish => { this.dish = dish; this.dishcopy = dish;
-                          this.setPrevNext(dish.id); },
+                          this.setPrevNext(dish.id); 
+                          this.visibility = 'shown'; },
                 errmess => this.errMess = <any>errmess);
 
     let id = +this.route.snapshot.params['id'];
